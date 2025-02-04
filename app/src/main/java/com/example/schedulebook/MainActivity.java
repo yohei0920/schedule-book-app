ab5b6ef39531bf950712619bedf0ec906c2b570e
yohei.hokari <yohei.hokari@gmail.com>
2025-02-04 21:30:23 +0900

    first commit

---
+++ app/src/main/java/com/example/schedulebook/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.schedulebook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button db_btn = (Button) findViewById(R.id.db);
+        db_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RealmTestActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
