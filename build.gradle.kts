115d087fe1f99e539d2dbd20729a131daef4c119
yohei.hokari <yohei.hokari@gmail.com>
2025-02-05 22:13:39 +0900

    wip: realm依存関係の追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("io.realm:realm-gradle-plugin:10.18.0")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
 }
